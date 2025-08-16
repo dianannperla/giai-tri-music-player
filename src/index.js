cbdee8c3c4f2ce08d2865f2dc968d71bb7695585
dianannperla <waafirmelyza@hotmail.com>
2025-08-16 16:33:51 +0100

    feat: initial commit for src/index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { MusicProvider } from './context/MusicContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <MusicProvider>
+    <App />
+  </MusicProvider>
+);
