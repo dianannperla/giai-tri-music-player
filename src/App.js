3b4acf00fbff55e733ef164fc551f20d47a8bd31
dianannperla <waafirmelyza@hotmail.com>
2025-08-16 16:33:53 +0100

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MusicPlayer from './components/MusicPlayer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Giai Tri Music Player</h1>
+      <MusicPlayer />
+    </div>
+  );
+}
+
+export default App;
